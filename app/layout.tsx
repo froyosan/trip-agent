31e4a5f349d6f62d6eb25b6f3c4ca24af39bd40b
froyosan <harpunesy@gmail.com>
2025-07-13 11:36:50 +0700

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from "next";
+import { Raleway } from "next/font/google";
+import NavBar from "@/components/organisme/navbar";
+import Footer from "@/components/footer";
+import "./globals.css";
+
+
+const raleway = Raleway ({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Home",
+  description: "Website Testing JAMETOL",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${raleway.variable} antialiased`}>
+        <NavBar/>
+        <main className="bg-gray-50 min-h-screen">{children}</main>
+        <Footer/>
+      </body>
+    </html>
+  );
+}
